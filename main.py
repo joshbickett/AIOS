b8015f2bd9722b5102ef6eafe189e94ad7f86171
Josh Bickett <josh@bickett.net>
2024-02-10 11:13:36 -0800

    Add `python-xlib` and `main.py` small changes

--- main.py
+++ main.py
@@ -7,21 +7,29 @@ class DoubleCmdListener:
     def __init__(self):
         self.cmd_pressed = False
         self.last_cmd_time = 0
-        self.time_threshold = 0.5  # Time threshold in seconds for double press
+        self.time_threshold = 0.2  # Time threshold in seconds for double press
+        self.last_key = None
 
     def on_press(self, key):
-        if key == keyboard.Key.cmd:  # Change to keyboard.Key.ctrl on Windows/Linux
+        current_time = time.time()
+        if key == keyboard.Key.cmd:
+            print("cmd pressed")
             if (
                 self.cmd_pressed
-                and (time.time() - self.last_cmd_time) < self.time_threshold
+                and current_time - self.last_cmd_time <= self.time_threshold
+                and key
+                == self.last_key  # Check if the last key is the same as the current key
             ):
-                print("Double 'Cmd' pressed!")
+                print("DOUBLE 'Cmd' pressed!")
                 take_snippet_and_save()
-
                 self.cmd_pressed = False  # Reset after detection
             else:
                 self.cmd_pressed = True
-                self.last_cmd_time = time.time()
+                self.last_cmd_time = current_time
+                self.last_key = key  # Save the last key pressed
+        else:
+            print("reset because key press: ", key)
+            self.cmd_pressed = False  # Reset if any other key is pressed
 
     def on_release(self, key):
         pass
